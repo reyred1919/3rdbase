b97506560f72da409805585caaf21cdde3a0a7e1
Adbuzz <adbuzz2023@gmail.com>
2025-09-01 11:27:09 +0000

    مشکل داریم توی route  چون باید 5 بار .. باشه تو 4 بار زدی

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 // This file is required for NextAuth.js to handle its API routes.
 // It exports the GET and POST handlers from your main auth.ts configuration.
 
-import { GET, POST } from "../../../../auth"
+import { GET, POST } from "../../../../../auth"
 export { GET, POST }
